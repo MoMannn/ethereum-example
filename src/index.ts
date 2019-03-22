96ce239933fe2097940e3a5c452fbea5ea5c6283
Tadej Vengust <vengust.tadej@gmail.com>
2019-03-22 12:45:24 +0100

    Add code comments.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+// Importing everything.
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { HttpProvider } from '@0xcert/ethereum-http-provider';
 import { AssetLedger } from '@0xcert/ethereum-asset-ledger';
 
+// Setup Express and body parser.
 const app = express();
 app.use(bodyParser.urlencoded({
   extended: true
@@ -10,26 +12,31 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 const port = 3000;
 
+// Instance of provider (this uses my ethereum address as accountId, please replace with you own).
 const provider = new HttpProvider({
   url: 'http://127.0.0.1:8545',
   accountId: '0x43813a78436c9ce02c97dd93ccd0ba33618f379b',
   requiredConfirmations: 1
 });
 
+// Default route.
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
+// Deploy a new asset ledger.
 app.post('/deploy', async (req, res) => {
+  // I'm adding CORS headers to allow this API to be used in index.html open from file system.
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
   const mutation = await AssetLedger.deploy(provider, {
     name: req.body.name,
     symbol: req.body.symbol,
     uriBase: req.body.uriBase,
     schemaId: req.body.schemaId,
     capabilities: req.body.capabilities
-  });
+  }); // You can catch errors by adding .catch((e) => console.log(e))
   res.send(mutation.id);
 });
 
